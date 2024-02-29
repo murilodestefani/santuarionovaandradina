860745a0972df7a84c9cbb15e5c1992bd1505f75
MuriloDestefani <murilo_destefani@hotmail.com>
2024-02-29 12:57:41 -0400

    update

--- src/Oracao.tsx
+++ src/Oracao.tsx
@@ -2,29 +2,54 @@ import { Image } from "@nextui-org/react";
 
 export function Oracao() {
   return (
-    <section id="oracao" className="flex min-h-svh items-center justify-center">
-      <div className="container flex flex-col gap-2 justify-center items-center px-4 md:flex-row-reverse">
-        <Image className="h-72 lg:h-80 xl:h-96" src="/assets/img/imaculado.png" />
+    <section id="oracao" className="flex min-h-svh items-center justify-center py-8">
+      <div className="container flex flex-col items-center justify-center gap-2 px-4 md:flex-row-reverse">
+        <Image
+          className="h-72 md:h-screen"
+          src="/assets/img/imaculado.png"
+        />
         <div>
-          <h2 className="text-center text-2xl font-bold text-penn-red-900 md:text-3xl lg:text-4xl">
-            Consagração
-          </h2>
+          <div className="flex flex-col items-center justify-center">
+            <h2 className="text-center text-xl font-bold uppercase text-penn-red-900">
+              Ato de Consagração ao
+            </h2>
+            <span className="text-center text-xl font-bold uppercase text-penn-red-900">
+              Imaculado Coração de Maria
+            </span>
+          </div>
+
           <p className="mt-1 text-center text-tiny font-semibold lg:text-sm">
-            Ó Coração Imaculado de Maria,
-            <br /> Repleto de bondade, mostrai-nos o Vosso amor. <br />A chama
-            do vosso Coração, ó Maria, desça sobre todos os homens! <br />
-            Nós Vos amamos infinitamente! <br />
-            Imprimi nos nossos corações o verdadeiro amor, <br />
-            para que sintamos o desejo de Vos buscar incessantemente. <br />Ó
-            Maria, Vós que tendes um Coração suave e humilde <br />
-            lembrai-vos de nós quando caírmos no pecado. <br />
-            Vós sabeis que todos os homens pecam. <br />
-            Concedei que, por meio de Vosso Imaculado e Materno Coração, <br />
-            sejamos curados de toda doença espiritual. <br />
-            Fazei que possamos sempre contemplar a bondade de Vosso Materno
-            Coração
-            <br />e nos convertamos por meio da chama do Vosso Coração. <br />
-            Amém.
+            Pedimos vossa permissão ó maria, <br />
+            para nos aproximarmos <br />
+            do vosso imaculado coração <br />
+            e contemplarmos vossa amorosa presença <br />
+            <br />
+            Pedimos vossa compaixão, <br />
+            ó mãe bondosa, <br />
+            para entrarmos humildemente <br />
+            em vosso santuário, <br />
+            mesmo com nossos erros e pecados. <br />
+            <br />
+            Pedimos vossa intercessão, <br />
+            virgem piedosa, <br />
+            para nos sentirmos <br />
+            amados e poder ser reconciliados <br />
+            com teu filho jesus. <br />
+            <br />
+            Na vossa presença ó imaculado <br />
+            coração de maria, <br />
+            consagramos-te nossa vida, <br />
+            nossas famílias que estão em casa <br />e da nossa diocese. <br />
+            <br />
+            Pedimos vossa proteção coração <br />
+            de maria, sede o nosso refúgio, <br />
+            dai-nos sustento para a igreja <br />e a humanidade. <br />
+            <br />E pedimos vossa benção, <br />ó maria, para sairmos daqui{" "}
+            <br />
+            cheios do espirito santo <br />e sermos missionários <br />a teu
+            exemplo para a gloria do pai.
+            <br />
+            <span className="font-bold text-penn-red-900">Amém</span>
           </p>
         </div>
       </div>
