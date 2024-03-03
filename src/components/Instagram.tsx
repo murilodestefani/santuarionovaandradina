3908d94bf4a92b3d09d421e7a2f04497b8d2e50b
murilodestefani <murilo_destefani@hotmail.com>
2024-03-03 17:52:26 -0400

    update

---
+++ src/components/Instagram.tsx
@@ -0,0 +1,39 @@
+import { Link, Image } from "@nextui-org/react";
+
+export function Instagram() {
+  return (
+    <section
+      id="instagram"
+      className="flex flex-col items-center justify-center gap-2"
+    >
+      <Link
+        isExternal
+        color="foreground"
+        className="text-tiny font-bold text-shark-950"
+        href="https://www.instagram.com/santuarionovaandradina/"
+      >
+        @santuarionovaandradina
+      </Link>
+      <div className="flex w-full justify-center">
+        <Image radius="none" src="./assets/img/instagram/01.png" />
+        <Image radius="none" src="./assets/img/instagram/02.png" />
+        <Image radius="none" src="./assets/img/instagram/03.png" />
+        <Image
+          className="hidden md:block"
+          radius="none"
+          src="./assets/img/instagram/04.png"
+        />
+        <Image
+          className="hidden lg:block"
+          radius="none"
+          src="./assets/img/instagram/05.png"
+        />
+        <Image
+          className="hidden xl:block"
+          radius="none"
+          src="./assets/img/instagram/06.png"
+        />
+      </div>
+    </section>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { Home } from "./pages/home";
 import "./index.css";
 import { NextUIProvider } from "@nextui-org/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <NextUIProvider>
     <React.StrictMode>
-      <App />
+      <Home />
     </React.StrictMode>
   </NextUIProvider>,
 );
